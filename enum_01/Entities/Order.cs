1fe2bb40ac60d3b6a3d0932e4ccf34d2f64b91f4
José Augusto Dias Mome <josemome@hotmail.com>
2020-09-13 15:43:34 -0300

    Exercicio de enumeracao em c-sharp, versao inicia.

---
+++ enum_01/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using enum_01.Entities.Enums;
+
+namespace enum_01.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                +Status;
+        }
+    }
+}
---
+++ enum_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using enum_01.Entities;
+using enum_01.Entities.Enums;
+using System;
+
+namespace enum_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}
